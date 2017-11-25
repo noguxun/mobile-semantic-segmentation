6c8c7d5a34feb17af8fd9097f60c63ab4164e59c
noguxu <noguxu@gmail.com>
2017-11-25 20:29:47 +0900

    add a script to predict one file

--- pred.py
+++ pred.py
@@ -12,19 +12,17 @@ from data import seed, standardize
 from loss import np_dice_coef
 from nets.MobileUNet import custom_objects
 
-SAVED_MODEL1 = 'artifacts/224_1_1.h5'
-SAVED_MODEL2 = 'artifacts/224_1_1.h5'
+SAVED_MODEL1 = 'artifacts/model.h5'
 
-size = 224
+size = 128
 
 
 def main():
     with CustomObjectScope(custom_objects()):
         model1 = load_model(SAVED_MODEL1)
-        model2 = load_model(SAVED_MODEL2)
 
-    images = np.load('data/images-224.npy')
-    masks = np.load('data/masks-224.npy')
+    images = np.load('data/images-128.npy')
+    masks = np.load('data/masks-128.npy')
     # only hair
     masks = masks[:, :, :, 0].reshape(-1, size, size)
 
@@ -42,16 +40,9 @@ def main():
         elapsed = time.time() - t1
         print('elapsed1: ', elapsed)
 
-        t1 = time.time()
-        pred2 = model2.predict(standardize(batched2)).reshape(size, size)
-        elapsed = time.time() - t1
-        print('elapsed2: ', elapsed)
-
         dice = np_dice_coef(mask.astype(float) / 255, pred1)
         print('dice1: ', dice)
 
-        dice = np_dice_coef(mask.astype(float) / 255, pred2)
-        print('dice2: ', dice)
 
         if True:
             plt.subplot(2, 2, 1)
@@ -60,9 +51,9 @@ def main():
             plt.imshow(mask)
             plt.subplot(2, 2, 3)
             plt.imshow(pred1)
-            plt.subplot(2, 2, 4)
-            plt.imshow(pred2)
             plt.show()
+
+        #break
 
 
 if __name__ == '__main__':
---
+++ pred_one_image.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+from keras.models import load_model
+from keras.utils import CustomObjectScope
+from sklearn.model_selection import train_test_split
+from scipy.misc import imresize
+from scipy.ndimage import imread
+
+from data import seed, standardize
+from loss import np_dice_coef
+from nets.MobileUNet import custom_objects
+
+SAVED_MODEL1 = 'artifacts/model.h5'
+
+img_size = 128
+
+
+def main(img_file):
+    with CustomObjectScope(custom_objects()):
+        model1 = load_model(SAVED_MODEL1)
+
+    img = imread(img_file)
+    img = imresize(img, (img_size, img_size))
+
+    batched1 = img.reshape(1, img_size, img_size, 3).astype(float)
+    pred1 = model1.predict(standardize(batched1)).reshape(img_size, img_size)
+
+    if True:
+        plt.subplot(2, 2, 1)
+        plt.imshow(img)
+        plt.subplot(2, 2, 2)
+        plt.imshow(pred1)
+        plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--img_file',
+        type=str,
+        default='data/test.jpg',
+    )
+
+    args, _ = parser.parse_known_args()
+    main(**vars(args))
